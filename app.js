9b9135e15d5f2b77313603bcdb0df9447eafb532
GABRIEL SOUZA COSTA <24122537@CFP127ALUNO.LOCAL>
2025-02-13 16:40:23 -0300

    App ok

---
+++ app.js
@@ -0,0 +1,16 @@
+/*********************************************************************************************
+ 
+* Objetivo: API referente ao projeto de controle de jogos
+ * Autor: Gabriel Souza Costa
+ * Data: 13/02/2024
+ * Versão: 1.0
+ * Observação:
+ *********** Para configurar e instalar a API, precisamos das seguintes bibliotecas:
+ *                 express             npm install express --save
+ *                 cors                npm install cors --save
+ *                 body-parser         npm install body-parser --save
+ *
+ *********** Para configurar e Instalar o acesso ao Banco de Dados precisamos:
+                   prisma              npm install prisma --save (Conexão com o BD)
+                   prisma/client       npm install @prisma/client --save (Executa scripts no BD)
+ ********************************************************************************************/
